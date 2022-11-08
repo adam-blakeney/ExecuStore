2726070d6b87fc6ad3f617e656b1a692d6a993c9
adam-blakeney <adam.blakeney@outlook.com>
2022-11-08 14:40:47 +0000

    added all auth to urls.py

--- EStore/settings.py
+++ EStore/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -63,13 +63,14 @@ TEMPLATES = [
 
 
 AUTHENTICATION_BACKENDS = [
-    ...
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+SITE_ID = 1
 
 
 WSGI_APPLICATION = 'EStore.wsgi.application'
