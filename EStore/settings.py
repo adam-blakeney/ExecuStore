ebe4f476fe38df530c455a5a43580ce8be5b011c
adam-blakeney <adam.blakeney@outlook.com>
2022-11-12 16:12:55 +0000

    fixed syntax error in base.html

--- EStore/settings.py
+++ EStore/settings.py
@@ -26,11 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +48,10 @@ ROOT_URLCONF = 'EStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
