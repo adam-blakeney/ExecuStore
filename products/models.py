6dd03424a4cb4ee71fa2e5bf60af94f39b734c55
adam-blakeney <adam.blakeney@outlook.com>
2022-11-22 14:24:13 +0000

    starting to integrate categories to products, bugged

--- products/models.py
+++ products/models.py
@@ -3,7 +3,17 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.CharField(max_length=255)
