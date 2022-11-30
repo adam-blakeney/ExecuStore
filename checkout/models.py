33c9e0119a4d6bb84a1c6f9f92714ec3bbdd1d68
adam-blakeney <adam.blakeney@outlook.com>
2022-11-30 11:42:26 +0000

    added checkout templates and static

--- checkout/models.py
+++ checkout/models.py
@@ -63,7 +63,7 @@ class OrderLineItem(models.Model):
                               on_delete=models.CASCADE,
                               related_name='lineitems')
     product = models.ForeignKey(Product, null=False,
-                                 blank=False, on_delete=models.CASCADE)
+                                blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
                                          null=False, blank=False,
