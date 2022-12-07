6951cb69eeca7bcb38a76833201803f2e9ecf27a
adam-blakeney <adam.blakeney@outlook.com>
2022-12-07 13:33:47 +0000

    added mminor changes to try fi stripe 400 error

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -24,7 +24,7 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        
+       
         send_mail(
             subject,
             body,
@@ -43,9 +43,13 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        billing_details = intent.charges.data[0].billing_details
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2)
 
         for field, value in shipping_details.address.items():
             if value == "":
