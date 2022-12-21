a762814fe080be2fac74fd76ae6e993784a7fc97
adam-blakeney <adam.blakeney@outlook.com>
2022-12-21 17:25:48 +0000

    altered total

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,13 +12,13 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
-        if isinstance(item_data, int):
+        if isinstance(quantity, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
+            total += quantity * product.price
+            product_count += quantity
             bag_items.append({
                 'item_id': item_id,
-                'quantity': item_data,
+                'quantity': quantity,
                 'product': product,
             })
         else:
