5d623b60a94e3f8bec3e9975f7765f6cbaea7a7b
adam-blakeney <adam.blakeney@outlook.com>
2022-12-24 17:35:14 +0000

    miss capitalisation

--- EStore/urls.py
+++ EStore/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = 'Estore.views.handler404'
+
+handler404 = 'EStore.views.handler404'
