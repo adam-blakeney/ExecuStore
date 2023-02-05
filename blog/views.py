2fb11ca8d3e873960e6d7534223a177f9c1c4a1a
adam-blakeney <adam.blakeney@outlook.com>
2023-02-05 18:23:26 +0000

    added code for views.py in blog, allows usres to add edit and delete blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,107 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Blog
+from .forms import BlogForm
+
+
+@login_required
+def all_blogs(request):
+    """ View to show all blogs """
+    if not request.user.is_authenticated:
+        messages.error(request, 'Sorry, only staff can do that.')
+        return redirect(reverse('login'))
+
+    blog = Blog.objects.all()
+
+    context = {
+        'blog': blog,
+        'blog_detail': blog_detail,
+    }
+
+    return render(request, 'blog/blog.html', context)
+
+
+def blog_detail(request, blog_id):
+    """ A view for individual blog """
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
+
+
+@login_required
+def add_blog(request):
+    """ Add a blog to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only staff can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog = form.save()
+            messages.success(request, 'Successfully added blog!')
+            return redirect(reverse('blog_detail', args=[blog.id]))
+        else:
+            messages.error(request,
+                           'Failed to add blog. Ensure the form is valid.')
+    else:
+        form = BlogForm()
+
+    template = 'blog/add_blog.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_blog(request, blog_id):
+    """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only staff can do that.')
+        return redirect(reverse('home'))
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated blog!')
+            return redirect(reverse('blog_detail', args=[blog.id]))
+        else:
+            messages.error(request,
+                           'Failed to update blog. Ensure the form is valid.')
+    else:
+        form = BlogForm(instance=blog)
+        messages.info(request, f'You are editing {Blog.blog_title}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_blog(request, blog_id):
+    """ Delete a blog from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only staff can do that.')
+        return redirect(reverse('home'))
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+    blog.delete()
+    messages.success(request, 'Blog deleted!')
+    return redirect(reverse('blog'))
