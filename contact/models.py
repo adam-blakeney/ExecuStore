1e55e3f7bf23c72bd2130899c0047e8cce1410b6
adam-blakeney <adam.blakeney@outlook.com>
2023-03-11 10:16:51 +0000

    added models for contact form

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    second_name = models.CharField(max_length=50, null=False, blank=False)
+    contact_email = models.EmailField(max_length=100, null=False, blank=False)
+    subject = models.TextField(max_length=250, null=False, blank=False)
+
+    def __str__(self):
+        return self.first_name
