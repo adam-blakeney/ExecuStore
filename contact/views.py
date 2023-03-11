59b2bc4fc7b16b77343d4d5637fb458c67a765ba
adam-blakeney <adam.blakeney@outlook.com>
2023-03-11 15:06:29 +0000

    fixed views and added toasts for validation

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Contact
 from .forms import ContactForm
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from django.contrib import messages
+from django.conf import settings
 
 
 # Create your views here.
@@ -9,16 +11,11 @@ from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 
 def Contact(request):
     if request.method == "POST":
-        contact = Contact
-        first_name = request.POST.get('first_name')
-        second_name = request.POST.get('second_name')
-        contact_email = request.POST.get('contact_email')
-        subject = request.POST.get('subject')
-        contact.first_name = first_name
-        contact.second_name = second_name
-        contact.contact_email = contact_email
-        contact.subject = subject
-        contact.save()
-        return HttpResponse("success")
-        # return messages.success(request, 'Your message has been recieved!')
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been recieved!')
+        else:
+            messages.warning(request, 'Please fill out form fully')
+
     return render(request, 'contact.html')
